97ae60c79ce69d024089c192cfab25e1de95429e
RajulThakur <rajulthakur34@gmail.com>
2026-06-28 22:53:51 +0530

    feat: add @JsonProperty annotation to xUrl field in social link DTOs

--- src/main/java/com/Ishwarjit/Wolf_OVRN_backend/dto/SocialLinkResponse.java
+++ src/main/java/com/Ishwarjit/Wolf_OVRN_backend/dto/SocialLinkResponse.java
@@ -5,12 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class SocialLinkResponse {
     private String instagramUrl;
+    
+    @JsonProperty("xUrl")
     private String xUrl;
+    
     private String linkedinUrl;
 }
--- src/main/java/com/Ishwarjit/Wolf_OVRN_backend/dto/UpdateSocialLinkRequest.java
+++ src/main/java/com/Ishwarjit/Wolf_OVRN_backend/dto/UpdateSocialLinkRequest.java
@@ -2,9 +2,14 @@ package com.Ishwarjit.Wolf_OVRN_backend.dto;
 
 import lombok.Data;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Data
 public class UpdateSocialLinkRequest {
     private String instagramUrl;
+    
+    @JsonProperty("xUrl")
     private String xUrl;
+    
     private String linkedinUrl;
 }
